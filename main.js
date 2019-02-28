3e4815675dad26f24f2e5dbae5a5251aee3cb3a0
Source of 0 <SourceOf0@gmail.com>
2019-02-28 21:18:06 +0900

    init

---
+++ main.js
@@ -0,0 +1,33 @@
+const $element = $(".walk");
+const imagePath = "./destination";
+const totalFrames = 260;
+const timePerFrame = 24;
+var timeWhenLastUpdate;
+var timeFromLastUpdate;
+var frameNumber = 1;
+
+function getFramePath(i) {
+  return imagePath + "/walk_" + "00000".substr(0, 5 - i.toString().length) + i + ".svg";
+}
+
+function step(startTime) {
+  if (!timeWhenLastUpdate) timeWhenLastUpdate = startTime;
+  timeFromLastUpdate = startTime - timeWhenLastUpdate;
+  if (timeFromLastUpdate > timePerFrame) {
+    $element.attr("xlink:href", "#Frame_" + frameNumber);
+    timeWhenLastUpdate = startTime;
+    
+    if (frameNumber >= totalFrames) {
+      frameNumber = 1;
+    } else {
+      frameNumber = frameNumber + 1;
+    }
+  }
+  
+  requestAnimationFrame(step);
+}
+
+$(window).on("load", function() {
+  requestAnimationFrame(step);
+});
+
